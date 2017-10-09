596915c3805e5d770408cfe39778bee51d942d1f
Eric Yap <ericywl3@live.com>
2017-10-09 09:19:39 +0800

    Added new pset

---
+++ week05/pset03/src/main/java/Part_A/Calculator.java
@@ -0,0 +1,21 @@
+package Part_A;
+
+public class Calculator {
+    public static int add(int number1, int number2) {
+        // Enter your code here
+    }
+
+    public static int sub(int number1, int number2) {
+        // Enter your code here
+    }
+
+    public static int mul(int number1, int number2) {
+          // Enter your code here
+    }
+
+    // Integer divide. Return a truncated int.
+    public static int divInt(int number1, int number2) {
+         // Enter your code here
+    }
+}
+
---
+++ week05/pset03/src/main/java/Part_A/FindX.java
@@ -0,0 +1,20 @@
+package Part_A;
+
+public class FindX {
+    private int visitnumber = 0;
+    public FindX () {
+        visitnumber++;
+    }
+
+    public int getVisitNumber() {
+        return visitnumber;
+    }
+    public int findX(int x, int[] A) {
+        visitnumber++;
+        int len = A.length;
+        for (int i = 0; i < len; i++) {
+            if (A[i] == x) return i;
+        }
+        return -1;
+    }
+}
