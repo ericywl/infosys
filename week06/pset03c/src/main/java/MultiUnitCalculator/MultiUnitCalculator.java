de57ed6109740910d110798bd3531d0372ecadcd
ericywl <midnight2903@gmail.com>
2017-10-22 02:28:51 +0800

    unnecessary changes

--- week06/pset03c/src/main/java/MultiUnitCalculator/MultiUnitCalculator.java
+++ week06/pset03c/src/main/java/MultiUnitCalculator/MultiUnitCalculator.java
@@ -10,9 +10,8 @@ import java.io.InputStreamReader;
 public class MultiUnitCalculator {
 
 	/**
-	 * @param expression
-	 *            a String representing a multi-unit expression, as defined in
-	 *            the problem set
+	 * @param expression: a String representing a multi-unit expression, as defined in
+     *                    the problem set
 	 * @return the value of the expression, as a number possibly followed by
 	 *         units, e.g. "72pt", "3", or "4.882in"
 	 */
@@ -26,8 +25,7 @@ public class MultiUnitCalculator {
 	 * Repeatedly reads expressions from the console, and outputs the results of
 	 * evaluating them. Inputting an empty line will terminate the program.
 	 * 
-	 * @param args
-	 *            unused
+	 * @param args: unused
 	 */
 	public static void main(String[] args) throws IOException {
 		MultiUnitCalculator calculator;
