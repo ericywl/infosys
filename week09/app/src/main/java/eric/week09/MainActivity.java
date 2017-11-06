bb7bdd7cc19625652315232ac4ef888527af24ae
ericywl <midnight2903@gmail.com>
2017-11-06 16:40:49 +0800

    sutdlogo

--- week09/app/src/main/java/eric/week09/MainActivity.java
+++ week09/app/src/main/java/eric/week09/MainActivity.java
@@ -2,12 +2,32 @@ package eric.week09;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button sutdButton;
+    TextView sutdAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sutdAddress = findViewById(R.id.sutdAddress);
+        sutdAddress.setVisibility(View.INVISIBLE);
+
+        sutdButton = findViewById(R.id.sutdButton);
+    }
+
+    protected void whenClick(View view) {
+        if (view.getVisibility() == View.INVISIBLE) {
+            view.setVisibility(View.VISIBLE);
+            sutdButton.setText(R.string.buttonText2);
+        } else {
+            view.setVisibility(View.INVISIBLE);
+            sutdButton.setText(R.string.buttonText);
+        }
     }
 }
