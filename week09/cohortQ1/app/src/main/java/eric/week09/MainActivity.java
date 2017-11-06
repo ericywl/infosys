13a09bb388f04c5ba2bc50ed6263be74ebd7ff20
ericywl <midnight2903@gmail.com>
2017-11-06 23:10:54 +0800

    tallyCounter done

--- week09/cohortQ1/app/src/main/java/eric/week09/MainActivity.java
+++ week09/cohortQ1/app/src/main/java/eric/week09/MainActivity.java
@@ -1,13 +1,18 @@
 package eric.week09;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     TextView sutdAddress;
+    Button sutdButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +22,43 @@ public class MainActivity extends AppCompatActivity {
         sutdAddress = findViewById(R.id.sutdAddress);
         sutdAddress.setVisibility(View.INVISIBLE);
 
-        final Button sutdButton = findViewById(R.id.sutdButton);
+        sutdButton = findViewById(R.id.sutdButton);
         sutdButton.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                if (sutdAddress.getVisibility() == View.INVISIBLE) {
-                    sutdAddress.setVisibility(View.VISIBLE);
-                    sutdButton.setText(R.string.buttonText2);
-                } else {
-                    sutdAddress.setVisibility(View.INVISIBLE);
-                    sutdButton.setText(R.string.buttonText);
-                }
+                shortClick();
             }
         });
+
+        sutdButton.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                longClick();
+                return true;
+            }
+        });
+    }
+
+    private void shortClick() {
+        if (sutdAddress.getVisibility() == View.INVISIBLE) {
+            sutdAddress.setVisibility(View.VISIBLE);
+            sutdButton.setText(R.string.buttonText2);
+        } else {
+            sutdAddress.setVisibility(View.INVISIBLE);
+            sutdButton.setText(R.string.buttonText);
+        }
+    }
+
+    private void longClick() {
+        String address = getString(R.string.uni_address);
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("addr", address);
+
+        if (clipboard != null) {
+            clipboard.setPrimaryClip(clip);
+
+            Context appCtx = getApplicationContext();
+            Toast.makeText(appCtx, "SUTD Address copied to clipboard.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- week09/tallyCounter/app/src/main/java/eric/tallycounter/MainActivity.java
+++ week09/tallyCounter/app/src/main/java/eric/tallycounter/MainActivity.java
@@ -2,12 +2,38 @@ package eric.tallycounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tally;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tally = findViewById(R.id.tally);
+
+        Button incrementButton = findViewById(R.id.increment);
+        Button resetButton = findViewById(R.id.reset);
+
+        incrementButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tallyCount = tally.getText().toString();
+                int tallyCountVal = Integer.parseInt(tallyCount);
+
+                tally.setText(String.valueOf(tallyCountVal + 1));
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tally.setText(String.valueOf(0));
+            }
+        });
     }
 }
